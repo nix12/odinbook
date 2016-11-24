58caddac52b1a721e51251e0ee76298bee18bdd2
Mario Goodwin <mr.goodwin@outlook.com>
2016-11-24 07:54:24 -0500

    Added user profile and redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations'}
   root 'static_pages#home'
-
-  devise_for :users
+  resources :users, only: [:show]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
