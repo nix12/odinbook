46784e800c451a085d7e1eb7c28f567898e45f38
Mario Goodwin <mr.goodwin@outlook.com>
2016-11-26 18:54:54 -0500

    heroku fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require semantic-ui
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
