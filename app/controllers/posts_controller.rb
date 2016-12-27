bdeaae124c49f7e94e18701bd0d8ec781c1ed195
Mario Goodwin <mr.goodwin@outlook.com>
2016-12-27 00:28:48 -0500

    fix images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,6 @@ class PostsController < ApplicationController
 	private
 
 		def post_params
-			params.require(:post).permit(:content, :image)
+			params.require(:post).permit(:content, image: [:image_file_name, :image_file_size, :image_content_type, :image_updated_at])
 		end
 end
